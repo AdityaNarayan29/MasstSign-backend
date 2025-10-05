3b6af3ddff1c4c56caa54459b6fa0d83e7562049
Aditya Narayan <adityanarayan29j@gmail.com>
2025-10-05 14:50:39 +0400

    feat: Cors setup updates

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Enable CORS for your frontend
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3001', // frontend URL
+    credentials: true, // needed if sending cookies
+  });
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+  await app.listen(port);
+  console.log(`Server running on http://localhost:${port}`);
 }
 bootstrap();
